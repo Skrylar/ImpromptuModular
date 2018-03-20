4786ecbd7f7052e4d21250cc997e81997defd94c
MarcBoule <marc.boule@etsmtl.ca>
2018-03-20 19:35:06 -0400

    Revert "allow writing sequencer while running"
    This reverts commit 6933a9ba95c43fa4b446dfb7251b4d13155d6e30.

--- src/WriteSeq32.cpp
+++ src/WriteSeq32.cpp
@@ -286,23 +286,6 @@ struct WriteSeq32 : Module {
 			}
 		}
 		
-		// Write
-		if (writeTrigger.process(params[WRITE_PARAM].value + inputs[WRITE_INPUT].value)) {
-			int index = (indexChannel == 3 ? indexStepStage : indexStep);
-			// CV
-			cv[indexChannel][index] = quantize(inputs[CV_INPUT].value, params[QUANTIZE_PARAM].value > 0.5f);
-			// Gate
-			if (inputs[GATE_INPUT].active)
-				gates[indexChannel][index] = (inputs[GATE_INPUT].value >= 1.0f) ? true : false;
-			// Autostep
-			if (!running && (params[AUTOSTEP_PARAM].value > 0.5f) ) {
-				if (indexChannel == 3)
-					indexStepStage = moveIndex(indexStepStage, indexStepStage + 1, numSteps);
-				else 
-					indexStep = moveIndex(indexStep, indexStep + 1, numSteps);
-			}
-		}
-		
 		bool canEdit = !running || (indexChannel == 3);
 		if (canEdit)
 		{		
@@ -329,7 +312,22 @@ struct WriteSeq32 : Module {
 						indexStep = (i<<3) | (indexStep&0x7);
 				}
 			}
-
+			// Write
+			if (writeTrigger.process(params[WRITE_PARAM].value + inputs[WRITE_INPUT].value)) {
+				int index = (indexChannel == 3 ? indexStepStage : indexStep);
+				// CV
+				cv[indexChannel][index] = quantize(inputs[CV_INPUT].value, params[QUANTIZE_PARAM].value > 0.5f);
+				// Gate
+				if (inputs[GATE_INPUT].active)
+					gates[indexChannel][index] = (inputs[GATE_INPUT].value >= 1.0f) ? true : false;
+				// Autostep
+				if (params[AUTOSTEP_PARAM].value > 0.5f) {
+					if (indexChannel == 3)
+						indexStepStage = moveIndex(indexStepStage, indexStepStage + 1, numSteps);
+					else 
+						indexStep = moveIndex(indexStep, indexStep + 1, numSteps);
+				}
+			}
 		}
 		
 		// CV and gate outputs (staging area not used)
